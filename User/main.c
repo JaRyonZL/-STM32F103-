d4df597526e4c98d989e7170d5db86080b1ada76
BreezeJuvenile <202309057@huat.edu.cn>
2025-12-16 22:37:38 +0800

    交通灯与数码管混合测试：修改了数码管保持延时，但时间不准问题仍未解决

--- User/main.c
+++ User/main.c
@@ -16,6 +16,8 @@ int main(void)
 	TIM2_Init();
 	// 交通灯测试
 	TrafficLight_Init();
+	// 数码管测试
+	Segment_Init();
 
 	while (1)
 	{
@@ -23,7 +25,7 @@ int main(void)
 	}
 }
 
-void TrafficLight_Test(void)
+void TrafficLightSeg_Test(void)
 {
 	static uint32_t count = 0;
 	count++;
@@ -52,6 +54,32 @@ void TrafficLight_Test(void)
 	{
 		count = 0;
 	}
+
+	// 数码管显示倒计时
+	uint8_t snTime = 0;
+	uint8_t ewTime = 0;
+	if(count <= 5000)
+	{
+		snTime = (7000 - count) / 1000;
+		ewTime = 0;
+	}
+	else if(count <= 7000)
+	{
+		snTime = (7000 - count) / 1000;
+		ewTime = 0;
+	}
+	else if(count <= 12000)
+	{
+		snTime = 0;
+		ewTime = (14000 - count) / 1000;
+	}
+	else if(count <= 14000)
+	{
+		snTime = 0;
+		ewTime = (14000 - count) / 1000;
+	}
+	Segment_Display_Number(ewTime, snTime);
+
 }
 
 // TIM2中断处理程序
@@ -60,6 +88,6 @@ void TIM2_IRQHandler(void)
 	// 清除中断标志位
 	TIM2->SR &= ~TIM_SR_UIF;
 
-	TrafficLight_Test();
+	TrafficLightSeg_Test();
 
 }
